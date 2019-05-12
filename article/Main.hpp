b2bec685e9763edf913564a8787e4b4a9430b79e
gxj <940331234@qq.com>
2019-05-12 17:02:18 +0800

    zda

---
+++ article/Main.hpp
@@ -0,0 +1,14 @@
+//
+//  Main.hpp
+//  GraphCPro
+//
+//  Created by xiaojian Gu on 2019/5/8.
+//  Copyright © 2019 xiaojian Gu. All rights reserved.
+//
+
+#ifndef Main_hpp
+#define Main_hpp
+
+#include <stdio.h>
+
+#endif /* Main_hpp */
