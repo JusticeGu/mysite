b83aeb105efe63cf19bb7b5bed5a3b6d3ef5d9ab
gxj <940331234@qq.com>
2019-05-13 12:32:32 +0800

    功能增加/BUG修复

--- mysite/urls.py
+++ mysite/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
       path('image/',include('image.urls',namespace="image")),
       path(r'about/$',TemplateView.as_view(template_name="course/about.html"))
 
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
