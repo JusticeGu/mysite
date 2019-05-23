2ec71424aced54cc1668924e9bbfb8fbb0eaba90
gxj <940331234@qq.com>
2019-05-23 16:16:35 +0800

    功能增加/BUG修复 -0515

--- course/urls.py
+++ course/urls.py
@@ -1,14 +1,12 @@
-
-from django.conf.urls import url
-from django.urls import path
-
-from . import views
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import AboutView,CourseListView
+from .views import AboutView, CourseListView, ManageCourseListView, CreateCourseView, DeleteCourseView, CreateLessonView, ListLessonsView, DetailLessonView, StudentListLessonView
+
 app_name = "course"
 urlpatterns = [
     path('about/',AboutView.as_view(),name="about"),
     path('course-list/',CourseListView.as_view(),name="course-list"),
+    path('manage-course/',ManageCourseListView.as_view(),name="manage_course"),
+
 
      ]
--- course/views.py
+++ course/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from django.views.generic import TemplateView,ListView
-from .models import Course
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.base import TemplateResponseMixin
+from .models import Course, Lesson
+from .forms import CreateCourseForm, CreateLessonForm
+
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from braces.views import LoginRequiredMixin
+import json
 # Create your views here.
 class AboutView(TemplateView):
     template_name = "course/about.html"
@@ -10,3 +20,16 @@ class CourseListView(ListView):
     model = Course
     context_object_name = "course"
     template_name = 'course/course_list.html'
+
+class UserMixin:
+    def get_queryset(self):
+        qs = super(UserMixin, self).get_queryset()
+        return qs.filter(user=self.request.user)
+
+class UserCourseMixin(UserMixin, LoginRequiredMixin):
+    model = Course
+    login_url = "/account/login/"
+
+class ManageCourseListView(UserCourseMixin, ListView):
+    context_object_name = "courses"
+    template_name = 'course/manage/manage_course_list.html'
